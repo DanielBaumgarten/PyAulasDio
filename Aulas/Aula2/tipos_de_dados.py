85d272a75372cc732b0cfba5ad5469f6514b4982
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2024-04-19 07:06:57 -0300

    <chore>:<Aula referente a Tipo de Dados e Interatividade>

---
+++ Aulas/Aula2/tipos_de_dados.py
@@ -0,0 +1,24 @@
+#número inteiro - int()
+print(1+10)
+#número flutuante - float()
+print(1 + 2.5)
+#tipo booleano bool()
+print(True)
+print(False)
+#tipo String str()
+print("Pynthon")
+
+""" Modo Interátivo - No terminal python e enter, é possivel fazer cálculos, usar diretamente ou fazendo o print ()diretamente no terminal
+ Python  -i (acessar de forma direta)
+python -i nome_do_programa
+"""
+#Escopo local - Retorno a lista de nomes no escopo locoal atual
+dir()
+#Incluindo no escopo locol
+import math
+dir()
+#Lista de metódos que podem ser executados
+dir(100)
+
+#Help - quias são os argumentos que aquele metodo recebe, o que ele retorna e como que ele funciona
+help()
